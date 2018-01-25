912b1dd192c83baa0079c3392340829b7ba6fda8
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-01-25 18:48:58 +0100

    build: create deploy pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,44 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
+const gulp = require('gulp')
+const zip = require('gulp-zip')
+const install = require('gulp-install')
+const clean = require('gulp-clean')
 
-gulp.task('build', () =>
-    gulp.src('src/*')
-        .pipe(zip('miner-waker-lambda.zip'))
-        .pipe(gulp.dest('dist'))
-);
+const tmpDir = 'tmp'
+const srcDir = 'src'
+const distDir = 'dist'
+
+const copySrc = () => {
+  return gulp.src(`${srcDir}/**/*`)
+    .pipe(gulp.dest(`${tmpDir}/${srcDir}`))
+}
+
+const copyConfig = () => {
+  return gulp.src('config.yml')
+    .pipe(gulp.dest(`${tmpDir}`))
+}
+
+const installTask = () => {
+  return gulp.src('package.json')
+    .pipe(gulp.dest(tmpDir))
+    .pipe(install({
+      npm: '--production'
+    }))
+}
+
+const zipTask = () => {
+  return gulp.src(`${tmpDir}/**/*`, {
+    nodir: true
+  })
+    .pipe(zip('miner-waker-lambda.zip'))
+    .pipe(gulp.dest(distDir))
+}
+
+const cleanTask = () => {
+  return gulp.src('tmp').pipe(clean({
+    force: true
+  }))
+}
+
+gulp.task('build', (done) => {
+  gulp.series(copySrc, copyConfig, installTask, zipTask, cleanTask)(() => done())
+})
